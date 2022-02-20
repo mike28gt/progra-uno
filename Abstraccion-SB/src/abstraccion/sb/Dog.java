8296e5eebc039f4e79ec65cb8864dee61ab03266
mike28gt <miguelalejandro28@gmail.com>
2022-02-20 16:37:39 -0600

    Se agregan cambios en ejemplo de abstracción

---
+++ Abstraccion-SB/src/abstraccion/sb/Dog.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abstraccion.sb;
+
+/**
+ *
+ * @author miguelcatalan
+ */
+public class Dog {
+    int size;
+    String breed;
+    String name;
+    
+    void bark() {
+        System.out.println("Wauff! Wauf!");
+    }
+}
